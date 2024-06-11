d177adc8a8d878818ce3bb5b222b11b827ab52a3
mahajankapil005 <130069883+mahajankapil005@users.noreply.github.com>
2024-06-11 07:42:57 +0000

    done

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,8 +2,8 @@ import { json, response } from "express"
 import { asyncHandler } from "../utils/asyncHandler.js"
 
 const registerUser = asyncHandler(async (req, res) => {
-    return res.status(200, json({
-        message: "ok"
+     res.status(200, json({
+        message: "chai aur code"
     }))
 })
 
